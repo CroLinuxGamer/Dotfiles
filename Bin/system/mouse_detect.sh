085fb0568240d3c674d91dc919a6dc72b1e0a0ec
CroLinuxGamer <crolinuxgamer@gmail.com>
2020-06-24 11:20:07 +0200

    Added my mouse detection script.

---
+++ Bin/system/mouse_detect.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+lista=`xinput list | grep -i 'mouse'`
+listb=`xinput list | grep -i 'transceiver' | grep -i 'pointer'`
+listc=`xinput list | grep -i 'receiver' | grep -i 'pointer'`
+
+if [ ${#lista} -eq 0 ] && [ ${#listb} -eq 0 ] && [ ${#listc} -eq 0 ]; then
+    exec `xinput enable ETPS/2\ Elantech\ Touchpad`
+else
+    exec `xinput disable ETPS/2\ Elantech\ Touchpad`
+fi
