acf19a4e29552e3028545dc2a1bff7235a0e34be
CroLinuxGamer <crolinuxgamer@gmail.com>
2020-06-26 12:07:43 +0200

    Updated my configuration.

--- Bin/bspwm/floating.sh
+++ Bin/bspwm/floating.sh
@@ -4,8 +4,7 @@
 FLOATING_DESKTOP_ID=$(bspc query -D -d '^4')
 
 bspc subscribe node_add | while read -a msg ; do
-    desk_id=${msg[2]}
-    wid=${msg[4]}
-    [ "$FLOATING_DESKTOP_ID" = "$desk_id" ] && bspc node "$wid" -t floating
+   desk_id=${msg[2]}
+   wid=${msg[4]}
+   [ "$FLOATING_DESKTOP_ID" = "$desk_id" ] && bspc node "$wid" -t floating
 done
-
