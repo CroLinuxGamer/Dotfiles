abdab33291c7fa0baf2acbd4644e8e563e632774
CroLinuxGamer <crolinuxgamer@gmail.com>
2020-08-05 17:20:05 +0200

    Updated my scripts.

--- Bin/bspwm/autostart.sh
+++ Bin/bspwm/autostart.sh
@@ -12,19 +12,19 @@ sleep 0.1
 killall xfce4-power-manager
 
 # programs section
+floating.sh &
+sleep 0.1
 mouse_detect.sh &
 sleep 0.1
 xfce4-power-manager &
 sleep 0.1
-floating.sh &
+sxhkd -c ~/.config/sxhkd/sxhkdrc &
+sleep 0.1
+picom --experimental-backends &
 sleep 0.1
 feh --no-fehbg --bg-scale ~/Pictures/wallpaper.jpg &
 sleep 0.1
 polybar -r top &
 sleep 0.1
 polybar -r bottom &
-sleep 0.1
-sxhkd -c ~/.config/sxhkd/sxhkdrc &
-sleep 0.1
-picom --experimental-backends &
 sleep 0.1
