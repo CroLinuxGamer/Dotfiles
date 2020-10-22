2c434f5274223117c4112f23132e84b2bd3db8c2
CroLinuxGamer <crolinuxgamer@gmail.com>
2020-10-22 21:46:11 +0200

    Added new scrips.

---
+++ Bin/bspwm/quit.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+killall transmission-daemon
+killall dunst
+killall mpd
+killall /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1
+killlemonade
+killall floating.sh
+killall sxhkd
+killall picom
+bspc quit
